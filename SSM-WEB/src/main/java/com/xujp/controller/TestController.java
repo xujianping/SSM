8210570dde68ea23e29d81cb80d5d80390539a3c
xujp <304793680@qq.com>
2018-01-26 16:48:24 +0800

    集成了radis

--- SSM-WEB/src/main/java/com/xujp/controller/TestController.java
+++ SSM-WEB/src/main/java/com/xujp/controller/TestController.java
@@ -7,12 +7,14 @@ import com.xujp.service.GagService;
 import com.xujp.service.JedisClient;
 import com.xujp.service.ScoreService;
 import com.xujp.utils.BaseResult;
+import com.xujp.utils.JacksonUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,7 +22,7 @@ import java.util.List;
 /**
  * Created by Xujp on 2018/1/26.
  */
-@RestController
+@Controller
 @RequestMapping("/test")
 public class TestController {
     private static final Logger log = LogManager.getLogger(OrderController.class);
@@ -32,8 +34,8 @@ public class TestController {
     @Autowired
     private JedisClient jedisClient;
 
-
-    @RequestMapping(value = "/testMvc",produces="text/html;charset=UTF-8", method = {RequestMethod.GET, RequestMethod.GET})
+    @RequestMapping(value = "/testMvc",produces="application/json;charset=UTF-8", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
     public BaseResult test() {
         BaseResult result = new BaseResult();
         Score score = new Score();
@@ -53,29 +55,31 @@ public class TestController {
     }
 
 
-//    @RequestMapping(value = "/testRedis",produces="text/html;charset=UTF-8", method = {RequestMethod.GET,RequestMethod.GET})
-//    public String testRedis(Long id){
-//        List<Gag> gagList= null;
-//        try {
-//            String resulthget = jedisClient.hget("禁言表", id + "");
-//            if (resulthget != null) {
-//                //字符串转为list
-//                System.out.println("有缓存啦啦啦！！！");
+    @RequestMapping(value = "/testRedis", method = {RequestMethod.GET,RequestMethod.GET})
+    public BaseResult testRedis(Long id){
+        BaseResult result = new BaseResult();
+        List<Gag> gagList= null;
+        try {
+            String resulthget = jedisClient.hget("禁言表", id + "");
+            if (resulthget != null) {
+                //字符串转为list
+                System.out.println("有缓存啦啦啦！！！");
+//                gagList = JacksonUtil.json2list(resulthget,Gag);
 //                JSONArray array = JSONArray.parseArray(resulthget);
 //                gagList = (List) array;
-//            } else {
-//                System.out.println("禁言表没查过");
-//                gagList= gagService.findByUserId(id);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        try {
-//            String cacheString = JSON.toJSONString(gagList);
-//            jedisClient.hset("禁言表", id + "", cacheString);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return JSON.toJSONString(gagList);
-//    }
+            } else {
+                System.out.println("禁言表没查过");
+                gagList= gagService.findByUserId(id);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            jedisClient.hset("禁言表", id + "", gagList.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        result.setData(gagList);
+        return result;
+    }
 }
--- SSM-WEB/src/main/java/com/xujp/utils/JacksonUtil.java
+++ SSM-WEB/src/main/java/com/xujp/utils/JacksonUtil.java
@@ -1,114 +1,132 @@
 package com.xujp.utils;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
-import java.io.StringWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
- * 来源: https://www.oschina.net/code/snippet_2361282_53222
+ * 来源: http://blog.csdn.net/z69183787/article/details/48895077
  */
 public class JacksonUtil {
-    private static JacksonUtil ju;
-    private static JsonFactory jf;
-    private static ObjectMapper mapper;
 
-    private JacksonUtil() {
+    private static  Logger log = LogManager.getLogger(JacksonUtil.class);
+
+    private static final ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper();
+        //去掉默认的时间戳格式
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        //设置为中国上海时区
+        objectMapper.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+        objectMapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
+        //空值不序列化
+        objectMapper.setSerializationInclusion(Include.NON_NULL);
+        //反序列化时，属性不存在的兼容处理
+        objectMapper.getDeserializationConfig().withoutFeatures(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        //序列化时，日期的统一格式
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        //单引号处理
+        objectMapper.configure(com.fasterxml.jackson.core.JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
     }
 
-    /**
-     * 单例初始化
-     *
-     * @param @return
-     * @return JacksonUtil
-     * @throws
-     * @Title: newInstance
-     * @Description: TODO
-     */
-    public static JacksonUtil newInstance() {
-        if (ju == null) ju = new JacksonUtil();
-        return ju;
-    }
-
-    public static ObjectMapper getMapper() {
-        if (mapper == null) {
-            mapper = new ObjectMapper();
-            //配置jackson不检查json串跟java实体类字段的一一对应
-            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            //设置输入时忽略在JSON字符串中存在但Java对象实际没有的属性
-            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            //禁止使用int代表Enum的order()來反序列化Enum,非常危險
-            mapper.configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, true);
+    public static <T> T toObject(String json, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(json, clazz);
+        } catch (JsonParseException e) {
+            log.error(e.getMessage(), e);
+        } catch (JsonMappingException e) {
+            log.error(e.getMessage(), e);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
         }
-        return mapper;
+        return null;
     }
 
-    public static JsonFactory getFactory() {
-        if (jf == null) jf = new JsonFactory();
-        return jf;
+    public static <T> String toJson(T entity) {
+        try {
+            return objectMapper.writeValueAsString(entity);
+        } catch (JsonGenerationException e) {
+            log.error(e.getMessage(), e);
+        } catch (JsonMappingException e) {
+            log.error(e.getMessage(), e);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
     }
 
-
-    /**
-     * @param @param  obj ： java对象
-     * @param @return
-     * @return String
-     * @throws
-     * @Title: obj2json
-     * @Description: 把java对象转为json字符串
-     */
-    public String obj2json(Object obj) {
-        if (null != obj) {
-            JsonGenerator jg = null;
-            try {
-                jf = getFactory();
-                mapper = getMapper();
-                StringWriter out = new StringWriter();
-                jg = jf.createJsonGenerator(out);
-                mapper.writeValue(jg, obj);
-                return out.toString();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (jg != null) jg.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+    public static <T> T toCollection(String json, TypeReference<T> typeReference) {
+        try {
+            return objectMapper.readValue(json, typeReference);
+        } catch (JsonParseException e) {
+            log.error(e.getMessage(), e);
+        } catch (JsonMappingException e) {
+            log.error(e.getMessage(), e);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
         }
         return null;
     }
 
     /**
-     * @param @param  json ： json字符串换
-     * @param @param  clz ： java对象class
-     * @param @return
-     * @return Object
-     * @throws
-     * @Title: json2obj
-     * @Description: json字符串转换为java对象
+     * json string convert to map
      */
-    public <E> E json2obj(String json, Class<E> clz) {
-        if (null != json && !"".equals(json.trim())) {
-            try {
-                mapper = getMapper();
-                return mapper.readValue(json, clz);
-            } catch (JsonParseException e) {
-                e.printStackTrace();
-            } catch (JsonMappingException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
+    public static <T> Map<String, Object> json2map(String jsonStr)
+            throws Exception {
+        return objectMapper.readValue(jsonStr, Map.class);
     }
+
+    /**
+     * json string convert to map with javaBean
+     */
+    public static <T> Map<String, T> json2map(String jsonStr, Class<T> clazz)
+            throws Exception {
+        Map<String, Map<String, Object>> map = objectMapper.readValue(jsonStr,
+                new TypeReference<Map<String, T>>() {
+                });
+        Map<String, T> result = new HashMap<String, T>();
+        for (Map.Entry<String, Map<String, Object>> entry : map.entrySet()) {
+            result.put(entry.getKey(), map2pojo(entry.getValue(), clazz));
+        }
+        return result;
+    }
+
+    /**
+     * json array string convert to list with javaBean
+     */
+    public static <T> List<T> json2list(String jsonArrayStr, Class<T> clazz)
+            throws Exception {
+        List<Map<String, Object>> list = objectMapper.readValue(jsonArrayStr,
+                new TypeReference<List<T>>() {
+                });
+        List<T> result = new ArrayList<T>();
+        for (Map<String, Object> map : list) {
+            result.add(map2pojo(map, clazz));
+        }
+        return result;
+    }
+
+    /**
+     * map convert to javaBean
+     */
+    public static <T> T map2pojo(Map map, Class<T> clazz) {
+        return objectMapper.convertValue(map, clazz);
+    }
+
 }
